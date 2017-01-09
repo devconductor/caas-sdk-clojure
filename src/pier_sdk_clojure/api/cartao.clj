0ef081e2b50792e311d8642d811fc9474689e56d
Filipe Germano <filipe.germano@conductor.com.br>
2017-01-09 15:01:54 -0300

    SPL0005 - Preparando versão para homologação, incrementado da versão 2.3.1 para 2.4.0

--- src/pier_sdk_clojure/api/cartao.clj
+++ src/pier_sdk_clojure/api/cartao.clj
@@ -21,6 +21,25 @@
   [id-cartao id-status-impressao ]
   (:data (alterar-status-impressao-using-put-with-http-info id-cartao id-status-impressao)))
 
+(defn atribuir-pessoa-using-put-with-http-info
+  "Realiza a atribuiÃ§Ã£o de um cartÃ£o prÃ©-pago a uma pessoa.
+  Esta mÃ©todo tem como permite que um cartÃ£o de crÃ©dito impresso de forma avulsa e anÃ´nimo seja atribuÃ­do a uma pessoa para que esta passe a ser a portadora titular deste cartÃ£o."
+  [id-cartao id-pessoa ]
+  (call-api "/api/cartoes/{id_cartao}/atribuir-pessoa" :put
+            {:path-params   {"id_cartao" id-cartao }
+             :header-params {}
+             :query-params  {"id_pessoa" id-pessoa }
+             :form-params   {}
+             :content-types ["application/json"]
+             :accepts       ["application/json"]
+             :auth-names    ["access_token"]}))
+
+(defn atribuir-pessoa-using-put
+  "Realiza a atribuiÃ§Ã£o de um cartÃ£o prÃ©-pago a uma pessoa.
+  Esta mÃ©todo tem como permite que um cartÃ£o de crÃ©dito impresso de forma avulsa e anÃ´nimo seja atribuÃ­do a uma pessoa para que esta passe a ser a portadora titular deste cartÃ£o."
+  [id-cartao id-pessoa ]
+  (:data (atribuir-pessoa-using-put-with-http-info id-cartao id-pessoa)))
+
 (defn bloquear-using-put-with-http-info
   "Realiza o bloqueio de um determinado CartÃ£o
   Este mÃ©todo permite a realizaÃ§Ã£o do bloqueio (temporÃ¡rio) ou do cancelamento (definitivo) de um determinado cartÃ£o a partir do seu cÃ³digo de identificaÃ§Ã£o (id). Para isso, Ã© preciso informar qual o motivo deste bloqueio que nada mais Ã© do que atribuir um novo StatusCartao para ele dentre as opÃ§Ãµes praticadas pelo emissor."
@@ -39,6 +58,25 @@
   Este mÃ©todo permite a realizaÃ§Ã£o do bloqueio (temporÃ¡rio) ou do cancelamento (definitivo) de um determinado cartÃ£o a partir do seu cÃ³digo de identificaÃ§Ã£o (id). Para isso, Ã© preciso informar qual o motivo deste bloqueio que nada mais Ã© do que atribuir um novo StatusCartao para ele dentre as opÃ§Ãµes praticadas pelo emissor."
   [id-cartao id-status observacao ]
   (:data (bloquear-using-put-with-http-info id-cartao id-status observacao)))
+
+(defn cadastrar-alterar-senha-using-put-with-http-info
+  "Realiza o cadastro ou alteraÃ§Ã£o da senha de um CartÃ£o
+  Esta operaÃ§Ã£o tem como objetivo permitir que o portador de um determinado cartÃ£o possa definir uma senha, a sua escolha"
+  [id-cartao senha ]
+  (call-api "/api/cartoes/{id_cartao}/alterar-senha" :put
+            {:path-params   {}
+             :header-params {"senha" senha }
+             :query-params  {"id_cartao" id-cartao }
+             :form-params   {}
+             :content-types ["application/json"]
+             :accepts       ["application/json"]
+             :auth-names    ["access_token"]}))
+
+(defn cadastrar-alterar-senha-using-put
+  "Realiza o cadastro ou alteraÃ§Ã£o da senha de um CartÃ£o
+  Esta operaÃ§Ã£o tem como objetivo permitir que o portador de um determinado cartÃ£o possa definir uma senha, a sua escolha"
+  [id-cartao senha ]
+  (:data (cadastrar-alterar-senha-using-put-with-http-info id-cartao senha)))
 
 (defn consultar-limite-disponibilidade-using-get-with-http-info
   "Apresenta os limites do Portador do CartÃ£o
@@ -136,3 +174,98 @@
   ([] (listar-using-get nil))
   ([optional-params]
    (:data (listar-using-get-with-http-info optional-params))))
+
+(defn validar-cartao-chip-bandeirado-using-get-with-http-info
+  "Permite validar um CartÃ£o Bandeirado a partir do chip
+  Esta operaÃ§Ã£o tem como objetivo permitir que os Emissores validem a autenticidade de um determinado CartÃ£o a partir do envio dos dados sensÃ­veis impressos nele."
+  [numero-cartao criptograma ]
+  (call-api "/api/cartoes/bandeirados/validar/chip" :get
+            {:path-params   {}
+             :header-params {}
+             :query-params  {"numeroCartao" numero-cartao "criptograma" criptograma }
+             :form-params   {}
+             :content-types ["application/json"]
+             :accepts       ["application/json"]
+             :auth-names    ["access_token"]}))
+
+(defn validar-cartao-chip-bandeirado-using-get
+  "Permite validar um CartÃ£o Bandeirado a partir do chip
+  Esta operaÃ§Ã£o tem como objetivo permitir que os Emissores validem a autenticidade de um determinado CartÃ£o a partir do envio dos dados sensÃ­veis impressos nele."
+  [numero-cartao criptograma ]
+  (:data (validar-cartao-chip-bandeirado-using-get-with-http-info numero-cartao criptograma)))
+
+(defn validar-cartao-digitado-bandeirado-using-get-with-http-info
+  "Permite validar um CartÃ£o bandeirado a partir dos dados Impressos
+  Esta operaÃ§Ã£o tem como objetivo permitir que os Emissores validem a autenticidade de um determinado CartÃ£o a partir do envio dos dados sensÃ­veis impressos nele."
+  [numero-cartao nome-portador data-validade codigo-seguranca ]
+  (call-api "/api/cartoes/bandeirados/validar/digitado" :get
+            {:path-params   {}
+             :header-params {}
+             :query-params  {"numeroCartao" numero-cartao "nomePortador" nome-portador "dataValidade" data-validade "codigoSeguranca" codigo-seguranca }
+             :form-params   {}
+             :content-types ["application/json"]
+             :accepts       ["application/json"]
+             :auth-names    ["access_token"]}))
+
+(defn validar-cartao-digitado-bandeirado-using-get
+  "Permite validar um CartÃ£o bandeirado a partir dos dados Impressos
+  Esta operaÃ§Ã£o tem como objetivo permitir que os Emissores validem a autenticidade de um determinado CartÃ£o a partir do envio dos dados sensÃ­veis impressos nele."
+  [numero-cartao nome-portador data-validade codigo-seguranca ]
+  (:data (validar-cartao-digitado-bandeirado-using-get-with-http-info numero-cartao nome-portador data-validade codigo-seguranca)))
+
+(defn validar-cartao-digitado-nao-bandeirado-using-get-with-http-info
+  "Permite validar um CartÃ£o a partir dos dados Impressos
+  Esta operaÃ§Ã£o tem como objetivo permitir que os Emissores validem a autenticidade de um determinado CartÃ£o a partir do envio dos dados sensÃ­veis impressos nele."
+  [numero-cartao nome-portador data-validade codigo-seguranca ]
+  (call-api "/api/cartoes/nao-bandeirados/validar/digitado" :get
+            {:path-params   {}
+             :header-params {}
+             :query-params  {"numeroCartao" numero-cartao "nomePortador" nome-portador "dataValidade" data-validade "codigoSeguranca" codigo-seguranca }
+             :form-params   {}
+             :content-types ["application/json"]
+             :accepts       ["application/json"]
+             :auth-names    ["access_token"]}))
+
+(defn validar-cartao-digitado-nao-bandeirado-using-get
+  "Permite validar um CartÃ£o a partir dos dados Impressos
+  Esta operaÃ§Ã£o tem como objetivo permitir que os Emissores validem a autenticidade de um determinado CartÃ£o a partir do envio dos dados sensÃ­veis impressos nele."
+  [numero-cartao nome-portador data-validade codigo-seguranca ]
+  (:data (validar-cartao-digitado-nao-bandeirado-using-get-with-http-info numero-cartao nome-portador data-validade codigo-seguranca)))
+
+(defn validar-cartao-tarja-bandeirado-using-get-with-http-info
+  "Permite validar um CartÃ£o Bandeirado a partir da Tarja
+  Esta operaÃ§Ã£o tem como objetivo permitir que os Emissores validem a autenticidade de um determinado CartÃ£o a partir do envio dos dados sensÃ­veis impressos nele."
+  [numero-cartao trilha1 trilha2 ]
+  (call-api "/api/cartoes/bandeirados/validar/tarja" :get
+            {:path-params   {}
+             :header-params {}
+             :query-params  {"numeroCartao" numero-cartao "trilha1" trilha1 "trilha2" trilha2 }
+             :form-params   {}
+             :content-types ["application/json"]
+             :accepts       ["application/json"]
+             :auth-names    ["access_token"]}))
+
+(defn validar-cartao-tarja-bandeirado-using-get
+  "Permite validar um CartÃ£o Bandeirado a partir da Tarja
+  Esta operaÃ§Ã£o tem como objetivo permitir que os Emissores validem a autenticidade de um determinado CartÃ£o a partir do envio dos dados sensÃ­veis impressos nele."
+  [numero-cartao trilha1 trilha2 ]
+  (:data (validar-cartao-tarja-bandeirado-using-get-with-http-info numero-cartao trilha1 trilha2)))
+
+(defn validar-senha-using-post-with-http-info
+  "Permite validar a senha de um CartÃ£o
+  Esta operaÃ§Ã£o tem como objetivo permitir validar que a senha informada pelo portador de um determinado cartÃ£o estÃ¡ correta."
+  [id-cartao senha ]
+  (call-api "/api/cartoes/{id_cartao}/validar-senha" :post
+            {:path-params   {}
+             :header-params {"senha" senha }
+             :query-params  {"id_cartao" id-cartao }
+             :form-params   {}
+             :content-types ["application/json"]
+             :accepts       ["application/json"]
+             :auth-names    ["access_token"]}))
+
+(defn validar-senha-using-post
+  "Permite validar a senha de um CartÃ£o
+  Esta operaÃ§Ã£o tem como objetivo permitir validar que a senha informada pelo portador de um determinado cartÃ£o estÃ¡ correta."
+  [id-cartao senha ]
+  (:data (validar-senha-using-post-with-http-info id-cartao senha)))
