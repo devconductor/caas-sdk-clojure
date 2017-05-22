25957a6ee33d3ed4f1856c89aa99e806342e06d1
filipe.germano <filipe.germano@conductor.com.br>
2017-05-22 08:53:57 -0300

    versão 2.15.5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pier-sdk-clojure "2.15.0"
+(defproject pier-sdk-clojure "2.15.5"
   :description "GestÃ£o de pagamento eletrÃ´nicos como serviÃ§o"
   :url "http://pierlabs.io"
   :license {:name "Apache 2.0"
