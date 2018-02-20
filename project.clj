51ab4edc692cb584f5b7d95964a5ad337b11ef3b
devconductor <thiago.sampaio@conductor.com.br>
2018-02-20 17:00:09 -0300

    Atualização SDK

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pier-sdk-clojure "2.54.2"
+(defproject pier-sdk-clojure "2.54.4"
   :description "GestÃ£o de pagamento eletrÃ´nicos como serviÃ§o"
   :url "http://pierlabs.io"
   :license {:name "Apache 2.0"
